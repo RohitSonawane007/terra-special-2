80b24375ec4802b7b8e82ed5b1fbef2c7c932b1e
Rohit <rohitsonawane27719@gmail.com>
2026-09-24 17:22:02 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+module "ec2_webserver" {
+  source         = "./ec2-modules"
+  ami            = "ami-0fef201115eefe936"
+  instance_type  = "t3.micro"
+  name           = "server-1"
+}
+
+module "ec2-dbserver" {
+  source         = "./ec2-modules"
+  ami            = "ami-0b6d9d3d33ba97d99"
+  instance_type  = "t3.micro"
+  name           = "server-2"
+}
+
+output "module-level-webserver-public-ip" {
+	value = module.ec2_webserver.new-public-ip-test
+}
+output "module-level-dbserver-public-ip" {
+	value = module.ec2-dbserver.new-public-ip-test
+}
+output "module-level-webserver-private-ip" {
+	value = module.ec2_webserver.new-private-ip-test
+}
+output "module-level-dbserver-private-ip" {
+	value = module.ec2-dbserver.new-private-ip-test
+}
