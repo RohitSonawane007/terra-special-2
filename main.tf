b01bb0ac925670f4e2d4285d5901d68034aed0a3
Rohit <rohitsonawane27719@gmail.com>
2026-09-24 17:26:53 +0530

    my changes made in main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ module "ec2-dbserver" {
   source         = "./ec2-modules"
   ami            = "ami-0b6d9d3d33ba97d99"
   instance_type  = "t3.micro"
+  vpc_security_group_ids = [${aws_security_group.mysc.id}]
   name           = "server-2"
 }
 
